b9aa20ffd875fc830c409a7418e9804f53d2f659
Mandy Simpson <mandy@simpson.org.nz>
2019-10-13 21:05:32 +1300

    user effects done

--- MLProject.R
+++ MLProject.R
@@ -79,6 +79,7 @@ RMSE <- function(true_ratings, predicted_ratings){
 }
 
 # Base algorithm
+
 average_rating <- mean(edx$rating)
 average_rating
 
@@ -86,11 +87,17 @@ base_rmse <- RMSE(validation$rating, average_rating)
 base_rmse
 
 # Movie effects algorithm
+# calculate movie average difference to overall
+
 movie_avgs <- edx %>% 
   group_by(movieId) %>% 
   summarize(b_i = mean(rating - average_rating))
 
+#plot movie average difference to overall
+
 movie_avgs %>% qplot(b_i, geom ="histogram", bins = 10, data = ., color = I("black"))
+
+#use validation to make predictions and calculate RMSE
 
 movie_effects <- average_rating + validation %>% 
   left_join(movie_avgs, by='movieId') %>%
@@ -98,4 +105,32 @@ movie_effects <- average_rating + validation %>%
 movie_effects_rmse <- RMSE(movie_effects, validation$rating)
 movie_effects_rmse
 
+# User and movie effects algorithm
+#plot user averages
 
+edx %>% 
+  group_by(userId) %>% 
+  summarize(b_u = mean(rating)) %>% 
+  filter(n()>=100) %>%
+  ggplot(aes(b_u)) + 
+  geom_histogram(bins = 30, color = "black")
+
+# Calculate user average difference to overall and movie average
+
+user_avgs <- edx %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  group_by(userId) %>%
+  summarize(b_u = mean(rating - average_rating - b_i))
+
+#use validation to make predictions and caluclate RMSE
+
+user_movie_effects <- validation %>% 
+  left_join(movie_avgs, by='movieId') %>%
+  left_join(user_avgs, by='userId') %>%
+  mutate(pred = average_rating + b_i + b_u) %>%
+  .$pred
+
+user_movie_effects_rmse <- RMSE(user_movie_effects, validation$rating)
+user_movie_effects_rmse
+
+#Regularized algorithm
